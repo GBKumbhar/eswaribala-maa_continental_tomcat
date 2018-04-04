11784333f23fbddd92574ba088c97d75699247da
eswaribala <Parameswaribala@gmail.com>
2018-04-04 09:31:16 +0530

    Add files via upload

---
+++ tomcat_start.bat
@@ -0,0 +1,9 @@
+@echo off
+rem net Start tomcat8
+
+rem @echo off
+rem sc stop tomcat8 && sc start tomcat8
+cd
+Rem Changing the path to Program Files
+cd /D "C:\Program Files\Apache Software Foundation\Tomcat 8.0\bin"
+startup
---
+++ tomcat_stop.bat
@@ -0,0 +1,5 @@
+@echo off
+net stop tomcat8
+Rem Changing the path to Program Files
+cd /D "C:\Program Files\Apache Software Foundation\Tomcat 8.0\bin"
+shutdown
